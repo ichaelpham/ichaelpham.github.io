68d2758c04e870c74125057874b15e08439ddeb7
ichaelpham <pham.mdh@gmail.com>
2017-07-31 16:49:55 +1000

    arching more shit

---
+++ archive/food_fright/assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+      $('body').addClass('fuck');
+      $('.wrapper').addClass('fuck');
+      $('.address').addClass('fuck');
+      $('.address div img').attr("src","assets/images/map_2.png");
+      $('ul.content span + span + span').addClass('fuck');
+      $('ul.content span:nth-child(-n+2)').addClass('fuck');
+      $('ul#spec .cost').addClass('fuck');
+      $('.mid').addClass('fuck');
+      $('#mains').addClass('fuck');
+      $('#spec h1').addClass('fuck');
+      $('#spec').addClass('fuck');
+      $('.right').addClass('fuck');
+      $('.right .content:nth-child(1) h1').addClass('fuck');
+      $('.right .content:nth-child(2) h1').addClass('fuck');
+      $('.right div').addClass('fuck');
+      $('.msg').addClass('fuck');
+   });
